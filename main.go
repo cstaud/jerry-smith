1e89f05090081b45dbe294a541dc77c71a48692c
Christian Staude <christian.staude@staffbase.com>
2022-03-31 10:35:46 +0200

    add soem example content

--- main.go
+++ main.go
@@ -1,1 +1,82 @@
-package jerry_smith
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	slackMessenger := slack{}
+	teamsMessenger := teams{}
+	jeff := user{Name: "jeff"}
+	morty := user{Name: "morty"}
+
+	_ = send(slackMessenger, morty, "hi this is jeff")
+	_ = send(teamsMessenger, jeff, "hi this is morty")
+
+}
+
+func sendItTheUglyWay(messenger interface{}, receiver user, text string) error {
+	if len(text) > 20 {
+		return fmt.Errorf("message too long")
+	}
+	var err error
+	switch messenger.(type) {
+	case slack:
+		err = messenger.(*slack).deliver(receiver.Name, text)
+	case teams:
+		err = messenger.(*teams).deliver(receiver.Name, text)
+	default:
+		return fmt.Errorf("unknown messenger")
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type user struct {
+	Name string
+}
+
+// This is the function of interest.
+// send provides functionality to send a text via some messenger to a user.
+func send(messenger messenger, receiver user, text string) error {
+	// Some Business Logic
+	if len(text) > 20 {
+		return fmt.Errorf("message too long")
+	}
+
+	// Call some method I relay on. I know nothing about its implementation nor what messenger it could be.
+	err := messenger.deliver(receiver.Name, text)
+
+	// Some Business Logic
+	if err != nil {
+		fmt.Println("oh no what happened?")
+		return err
+	}
+	return nil
+}
+
+// The interface the send function relays on.
+type messenger interface {
+	deliver(receiver, text string) error
+}
+
+// Example implementation of a messenger
+type slack struct {
+}
+
+func (s slack) deliver(receiver, text string) error {
+	fmt.Printf("Sent: '%s' via SLACK to '%s'\n", text, receiver)
+	return nil
+}
+
+// Example implementation of a messenger
+type teams struct {
+}
+
+func (t teams) deliver(receiver, text string) error {
+	fmt.Printf("Sent: %s via TEAMS to %s\n", text, receiver)
+	return nil
+}
--- main_test.go
+++ main_test.go
@@ -1,1 +1,60 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_send(t *testing.T) {
+	messenger := messengerMock{}
+
+	tests := []struct {
+		name     string
+		text     string
+		receiver user
+		mocks    func()
+		wantErr  error
+	}{
+		{
+			name:     "error on too long text",
+			text:     "a much toooooooooooooooooooo long text",
+			receiver: user{Name: "dummy"},
+			mocks:    func() {},
+			wantErr:  fmt.Errorf("message too long"),
+		},
+		{
+			name:     "error on deliver",
+			text:     "some text",
+			receiver: user{Name: "dummy"},
+			mocks: func() {
+				messenger.ReturnErr = fmt.Errorf("could not deliver")
+			},
+			wantErr: fmt.Errorf("could not deliver"),
+		},
+		{
+			name:     "success",
+			text:     "some text",
+			receiver: user{Name: "dummy"},
+			mocks: func() {
+				messenger.ReturnErr = nil
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mocks()
+			err := send(messenger, tt.receiver, tt.text)
+			assert.Equal(t, tt.wantErr, err)
+		})
+	}
+}
+
+type messengerMock struct {
+	ReturnErr error
+}
+
+func (m messengerMock) deliver(receiver, text string) error {
+	return m.ReturnErr
+}
